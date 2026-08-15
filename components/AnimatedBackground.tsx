77d9229410a5393b803de228418923c690a6329b
Danil Belousov <danilbelousov758@gmail.com>
2026-08-16 02:29:23 +0300

    Add authenticated header

--- components/AnimatedBackground.tsx
+++ components/AnimatedBackground.tsx
@@ -9,64 +9,80 @@ export default function AnimatedBackground() {
                 inset-0
                 -z-0
                 overflow-hidden
-                bg-[#05070D]
+                bg-[#03050A]
             "
         >
+
+            {/* Яркий бело-синий центр */}
+            <div
+                className="
+                    absolute
+                    left-[-15%]
+                    top-[-20%]
+                    h-[750px]
+                    w-[750px]
+                    rounded-full
+                    bg-blue-500/40
+                    blur-[180px]
+                    animate-bg-1
+                "
+            />
+
+
+            {/* Справа яркий синий */}
+            <div
+                className="
+                    absolute
+                    right-[-10%]
+                    top-[10%]
+                    h-[700px]
+                    w-[700px]
+                    rounded-full
+                    bg-cyan-400/30
+                    blur-[170px]
+                    animate-bg-2
+                "
+            />
+
 
             {/* Белое свечение */}
             <div
                 className="
                     absolute
-                    left-[-10%]
-                    top-[-20%]
-                    h-[600px]
-                    w-[600px]
+                    left-[35%]
+                    top-[25%]
+                    h-[500px]
+                    w-[500px]
                     rounded-full
-                    bg-white/[0.12]
-                    blur-[140px]
-                    animate-background-one
-                "
-            />
-
-
-            {/* Ярко-синий шар */}
-            <div
-                className="
-                    absolute
-                    right-[-15%]
-                    top-[20%]
-                    h-[650px]
-                    w-[650px]
-                    rounded-full
-                    bg-blue-500/[0.18]
+                    bg-white/20
                     blur-[160px]
-                    animate-background-two
+                    animate-bg-3
                 "
             />
 
 
-            {/* Тёмно-синий */}
+            {/* Нижний глубокий синий */}
             <div
                 className="
                     absolute
-                    bottom-[-20%]
-                    left-[20%]
-                    h-[550px]
-                    w-[550px]
+                    bottom-[-25%]
+                    left-[15%]
+                    h-[800px]
+                    w-[800px]
                     rounded-full
-                    bg-blue-900/[0.35]
-                    blur-[170px]
-                    animate-background-three
+                    bg-blue-700/50
+                    blur-[200px]
+                    animate-bg-4
                 "
             />
 
 
-            {/* лёгкая сетка */}
+            {/* Тёмный слой для контраста */}
             <div
                 className="
                     absolute
                     inset-0
-                    bg-[radial-gradient(circle_at_top,rgba(255,255,255,0.08),transparent_35%)]
+                    bg-black/30
                 "
             />
 
